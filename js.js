c1cb6d74c22bfdde62e8afd00f442c447127dce6
Paco <38069658+pcoloc@users.noreply.github.com>
2021-02-02 16:16:07 +0100

    traducido

--- js.js
+++ js.js
@@ -28,56 +28,56 @@ function Img(){
         // Change position ONLY IF YOU KNOW WHAY YOU ARE DOING     "-123px"
         document.getElementById('img').style.backgroundPositionY = "-210px";
         // YOU CAN CHANGE YOUR NAME HERE               "Good morning Yourname!"
-        document.getElementById('welcome').innerHTML = "Good morning Paco!";
+        document.getElementById('welcome').innerHTML = "Buenos días Visitante!";
     }
     if(h>=12 && h<=16) {
         document.getElementById('img').style.backgroundImage = "url('img/day1.jpg')";
         document.getElementById('img').style.backgroundPositionY = "-250px";
         // ALSO CHANGE YOUR NAME HERE                  "Good morning Yourname!"
-        document.getElementById('welcome').innerHTML = "Good day Paco!";
+        document.getElementById('welcome').innerHTML = "Buen día Visitante!";
     }
     if(h>=17 && h<=19) {
         document.getElementById('img').style.backgroundImage = "url('img/noon1.jpg')";
         document.getElementById('img').style.backgroundPositionY = "-150px";
         // ALSO CHANGE YOUR NAME HERE                  "Good morning Yourname!"
-        document.getElementById('welcome').innerHTML = "Good afternoon Paco!";
+        document.getElementById('welcome').innerHTML = "Buenas tardes Visitante!";
     }
     if((h>=20 && h<=23) || (h>=0 && h<=5)){
         document.getElementById('img').style.backgroundImage = "url('img/night1.jpg')";
         document.getElementById('img').style.backgroundPositionY = "-240px";
         // ALSO CHANGE YOUR NAME HERE                  "Good morning Yourname!"
-        document.getElementById('welcome').innerHTML = "Good night Paco!";
+        document.getElementById('welcome').innerHTML = "Buenas noches Visitante!";
     }
 }
 function Datum(){
     var today = new Date();
 
     var weekday = new Array(7);
-    weekday[0]=  "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
+    weekday[0]=  "Domingo";
+    weekday[1] = "Lunes";
+    weekday[2] = "Martes";
+    weekday[3] = "It is Wednesday my dudes!";
+    weekday[4] = "Jueves";
+    weekday[5] = "Viernes";
+    weekday[6] = "Sábado";
 
     var day = weekday[today.getDay()];
 
     var d = today.getDate();
 
     var month = new Array(12);
-    month[0] = "January";
-    month[1] = "February";
-    month[2] = "March";
-    month[3] = "April";
-    month[4] = "May";
-    month[5] = "June";
-    month[6] = "July";
-    month[7] = "August";
-    month[8] = "September";
-    month[9] = "October";
-    month[10] = "November";
-    month[11] = "December";
+    month[0] = "Enero";
+    month[1] = "Febrero";
+    month[2] = "Marzo";
+    month[3] = "Abril";
+    month[4] = "Mayo";
+    month[5] = "Junio";
+    month[6] = "Julio";
+    month[7] = "Agosto";
+    month[8] = "Septiembre";
+    month[9] = "Octubre";
+    month[10] = "Noviembre";
+    month[11] = "Diciembre";
 
     var m = month[today.getUTCMonth()];
 
